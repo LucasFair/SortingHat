2d997a38df6d6fa978751a5e0b552a1ced31fee3
LucasFair <141710445+LucasFair@users.noreply.github.com>
2023-09-13 09:15:38 +0200

    Uploading files to repo.

---
+++ SortingHat/Model/HatHouse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SortingHat.Model
+{
+	/// <summary>
+	/// Class <c>HatHouse</c> contains the data for the Sorting Hat.
+	/// </summary>
+	public class HatHouse
+	{
+		enum Houses { Gryffindor, Hufflepuff, Ravenclaw, Slytherin }
+		public void Run()
+		{
+			Random rnd = new Random();
+			int hatSort;
+
+			for (int i = 0; i < 10; i++)
+			{
+				hatSort = rnd.Next(0, 4);
+				switch (hatSort)
+				{
+					case 0:
+						Console.ForegroundColor = ConsoleColor.Red;
+						break;
+					case 1:
+						Console.ForegroundColor = ConsoleColor.Yellow;
+						break;
+					case 2:
+						Console.ForegroundColor = ConsoleColor.Blue;
+						break;
+					case 3:
+						Console.ForegroundColor = ConsoleColor.Green;
+						break;
+				}
+				Console.WriteLine((Houses)hatSort);
+				Console.WriteLine(string.Empty);
+				Console.ResetColor();
+			}
+		}
+	}
+}
---
+++ SortingHat/Program.cs
@@ -0,0 +1,18 @@
+﻿using SortingHat.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHat
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			HatHouse hatHouse = new HatHouse();
+			hatHouse.Run();
+		}
+	}
+}
